a3bf34225a21adba3e95b8d1d4d5a82ba98bb8b0
hassanatuahmed <hassanatu@plendify.com>
2020-11-13 14:42:53 +0000

    update

---
+++ lib/business/employees.dart
@@ -0,0 +1,30 @@
+import 'package:fintech/business_pattern_view.dart';
+import 'package:fintech/second_pattern_view.dart';
+import 'package:flutter/material.dart';
+class Employees extends StatefulWidget {
+  @override
+  _EmployeesState createState() => _EmployeesState();
+}
+
+class _EmployeesState extends State<Employees> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          BusinessPatternView(
+              color: Colors.white,
+              color1: Colors.white,
+              color2: Colors.white,
+              color3: Colors.black,
+              color4: Colors.black,
+              color5: Colors.black,
+              color6: Colors.black,
+              color7: Colors.black,
+              color8: Colors.black,
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/business/identification_numbers.dart
+++ lib/business/identification_numbers.dart
@@ -1,5 +1,10 @@
 import 'package:fintech/business_pattern_view.dart';
+import 'package:fintech/create_account_screens/full_name.dart';
+import 'package:fintech/create_account_screens/pin_successfully_verified.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+import 'name.dart';
 
 class IdentificationNumbers extends StatefulWidget {
   @override
@@ -11,38 +16,120 @@ class _IdentificationNumbersState extends State<IdentificationNumbers> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          BusinessPatternView(
-            color: Colors.white,
-            color1: Colors.white,
-            color2: Colors.black,
-            color3: Colors.black,
-            color4: Colors.black,
-            color5: Colors.black,
-            color6: Colors.black,
-            color7: Colors.black,
-            color8: Colors.black,
-          ),
-          Text("Identification Numbers"),
-          Text("1 of 2",style: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontFamily: "Poppins",
-            fontStyle: FontStyle.normal,
-            fontSize: 14,
-            color: Colors.red,
-          ),),
-          Text("What is your Business Registration Number"),
-          Text("2 of 2",style: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontFamily: "Poppins",
-            fontStyle: FontStyle.normal,
-            fontSize: 14,
-            color: Colors.red,
-          ),),
-          Text("What is your Tax Identification Number"),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            BusinessPatternView(
+              color: Colors.white,
+              color1: Colors.white,
+              color2: Colors.black,
+              color3: Colors.black,
+              color4: Colors.black,
+              color5: Colors.black,
+              color6: Colors.black,
+              color7: Colors.black,
+              color8: Colors.black,
+            ),
 
-        ],
+            Padding(
+              padding: EdgeInsets.only(left:30.0),
+              child: Container(
+                height: 102,
+                width: 380,
+                child: Text("Identification\nNumbers",style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Poppins",
+                  fontStyle: FontStyle.normal,
+                  fontSize: 34,
+                ),),
+              ),
+            ),
+
+            Text("1 of 2",style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontFamily: "Poppins",
+              fontStyle: FontStyle.normal,
+              fontSize: 14,
+              color: Colors.red,
+            ),),
+
+            Container(
+              height: 60,
+                width: 341,
+                child: Text("What is your Business Registration Number",style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Poppins",
+                  fontStyle: FontStyle.normal,
+                  fontSize: 20,
+                ),),),
+
+            MyText(
+              hintText: "Number",
+              hintText_color:Colors.white ,
+              fillColor: HexColor("#333333"),
+            ),
+            Text("2 of 2",style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontFamily: "Poppins",
+              fontStyle: FontStyle.normal,
+              fontSize: 14,
+              color: Colors.red,
+            ),),
+
+            Padding(
+              padding: EdgeInsets.only(left:20.0),
+              child: Container(
+                width: 360,
+                  height: 60,
+                  child: Text("What is your Tax Identification \nNumber",style: TextStyle(
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "Poppins",
+                    fontStyle: FontStyle.normal,
+                    fontSize: 20,
+                  ),),),
+            ),
+            MyText(
+              hintText: "TIN",
+              hintText_color:Colors.white ,
+              fillColor: HexColor("#333333"),
+            ),
+
+            Row(
+              children: [
+                SizedBox(width: 36,),
+                Container(
+                  width: 125,
+                  height: 45,
+                  child: MyButton(
+                    BorderSide_color: Colors.black,
+                    text: "Sign Out",
+                    style:TextStyle(
+                      fontSize: 16,
+                      fontFamily: "Poppins",
+                      fontStyle: FontStyle.normal,
+                      color: Colors.black,
+                      fontWeight: FontWeight.w400,
+
+                    ),
+                    onPressed: (){},
+                  ),
+                ),
+                SizedBox(width: 45,),
+                CircleAvatar(
+                  backgroundColor: HexColor("333333").withOpacity(0.1),
+                  child: Text("?",style: TextStyle(color: Colors.black,
+                      fontSize: 25,
+                      fontFamily: "Poppins",
+                      fontWeight: FontWeight.w400,
+                      fontStyle: FontStyle.normal),),
+                ),
+                SizedBox(width: 80,),
+                NextArrow(),
+              ],
+            ),
+
+          ],
+        ),
       ),
     );
   }
