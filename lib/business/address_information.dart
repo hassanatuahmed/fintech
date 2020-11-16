44caf615b3b976bfe86d51616c322b855807acea
hassanatuahmed <hassanatu@plendify.com>
2020-11-16 10:59:50 +0000

    update

--- lib/business/address_information.dart
+++ lib/business/address_information.dart
@@ -19,108 +19,179 @@ class _BusinessAddressState extends State<BusinessAddress> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          BusinessPatternView(
-            color: Colors.white,
-            color1: Colors.white,
-            color2: Colors.white,
-            color3: Colors.white,
-            color4: Colors.white,
-            color5: Colors.white,
-            color6: Colors.white,
-            color7: Colors.black,
-            color8: Colors.black,
-          ),
-          Container(
-            height: 153,
-            width: 360,
-            child: Text("Address Information",style: TextStyle(
-              fontSize: 34,
-              fontStyle: FontStyle.normal,
-              fontFamily: "Poppins",
-              color: HexColor("333333").withOpacity(0.9),
-              fontWeight: FontWeight.w400,
-            ),),),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            BusinessPatternView(
+              color: Colors.white,
+              color1: Colors.white,
+              color2: Colors.white,
+              color3: Colors.white,
+              color4: Colors.white,
+              color5: Colors.white,
+              color6: Colors.white,
+              color7: Colors.black,
+              color8: Colors.black,
+            ),
+            Container(
+              height: 51,
+              width: 360,
+              child: Text("Address Information",style: TextStyle(
+                fontSize: 34,
+                fontStyle: FontStyle.normal,
+                fontFamily: "Poppins",
+                color: HexColor("333333").withOpacity(0.9),
+                fontWeight: FontWeight.w400,
+              ),),),
 
+             SizedBox(height: 35,),
+            Padding(
+              padding: EdgeInsets.only(right: 310.0),
+              child: Text("1 of 2",style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontFamily: "Poppins",
+                fontStyle: FontStyle.normal,
+                fontSize: 14,
+                color: Colors.red,
+              ),),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom:27.0,right: 50),
+              child: Text("Upload Proof of Business Address",style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.normal,
+                fontFamily: "Poppins",
+                fontWeight: FontWeight.w400,
+              ),),
+            ),
+            SizedBox(height: 26,),
 
-          SizedBox(height: 28,),
+            Padding(
+              padding: EdgeInsets.only(bottom:60.0,right: 150),
+              child: Container(
+                height: 200,
+                width: 186,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.black,width: 1),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                  children: [
 
-          Container(
-            height: 60,
-            width: 339,
-            decoration: BoxDecoration(
+                    Padding(
+                      padding: EdgeInsets.only(top:27.0,bottom:20.0),
+                      child: Image.asset("asset/images/bills.png",scale: 4,),
+                    ),
+                    Container(
+                      height: 48,
+                      width: 84,
+                      child: Text("Electricity/Water Bill",style: TextStyle(
+                        fontSize: 16,
+                        fontStyle: FontStyle.normal,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w400,
+                      ),
+                        textAlign: TextAlign.center,),
+                    ),
 
-                borderRadius: BorderRadius.circular(30.0),
-                color: HexColor("#333333"),
-                border: Border.all()),
-            child: Padding(
-              padding: EdgeInsets.only(right:8.0,left: 8.0),
-              child: DropdownButtonFormField<String>(
-
-                hint: Text("Business Type",style: TextStyle(
-                  fontWeight: FontWeight.w400,
-                  fontFamily: "Poppins",
-                  color: Colors.white,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 16,
-                ),),
-                value: selected,
-
-                items: ["three", "two", "one"]
-                    .map((label) => DropdownMenuItem(
-                  child: Text(label),
-                  value: label,
-                ))
-                    .toList(),
-                onChanged: (value) {
-                },
-
-
+                  ],
+                ),
               ),
             ),
-          ),
+            Padding(
+              padding: EdgeInsets.only(right: 310.0),
+              child: Text("2 of 2",style: TextStyle(
+                fontWeight: FontWeight.w400,
+                fontFamily: "Poppins",
+                fontStyle: FontStyle.normal,
+                fontSize: 14,
+                color: Colors.red,
+              ),),
+            ),
 
-          SizedBox(height: 100,),
+            Padding(
+              padding: EdgeInsets.only(right:10.0,bottom: 20),
+              child: Container(
+                height: 60,
+                width: 360,
+                child: Text("What’s your business’ digital \nAddress?",style: TextStyle(
+                  fontSize: 20,
+                  fontStyle: FontStyle.normal,
+                  fontFamily: "Poppins",
+                  fontWeight: FontWeight.w400,
 
+                ),),
+              ),
+            ),
 
-          Padding(
-            padding:  EdgeInsets.only(right:4.0),
-            child: Row(
-              children: [
-                SizedBox(width: 36,),
-                Container(
-                  width: 125,
-                  height: 45,
-                  child: MyButton(
-                    BorderSide_color: Colors.black,
-                    text: "Sign Out",
-                    style:TextStyle(
-                      fontSize: 16,
-                      fontFamily: "Poppins",
-                      fontStyle: FontStyle.normal,
-                      color: Colors.black,
-                      fontWeight: FontWeight.w400,
+            Container(
+              height: 60,
+              width: 339,
+              child: Container(
+                height: 60,
+                width: 339,
+                child: MaterialButton(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(30.0))),
+                  color: HexColor("#333333"),
+                  onPressed: (){},
+                  child: Row(
+                    children: [
+                      SizedBox(width: 15,),
 
-                    ),
-                    onPressed: (){},
+                      Text("Digital Address",style: TextStyle(
+                          fontWeight: FontWeight.w200,
+                          fontStyle: FontStyle.normal,
+                          fontFamily: "Poppins",
+                          color: Colors.white),),
+                      SizedBox(width: 160,),
+                      Icon(Icons.location_on,color: Colors.white,),
+                      SizedBox(width: 15,),
+
+                    ],
                   ),
                 ),
-                SizedBox(width: 45,),
-                CircleAvatar(
-                  backgroundColor: HexColor("333333").withOpacity(0.1),
-                  child: Text("?",style: TextStyle(color: Colors.black,
-                      fontSize: 25,
-                      fontFamily: "Poppins",
-                      fontWeight: FontWeight.w400,
-                      fontStyle: FontStyle.normal),),
-                ),
-                SizedBox(width: 80,),
-                NextArrow(),
-              ],
+              ),
             ),
-          ),
-        ],
+            SizedBox(height: 97,),
+
+            Padding(
+              padding:  EdgeInsets.only(right:4.0),
+              child: Row(
+                children: [
+                  SizedBox(width: 36,),
+                  Container(
+                    width: 125,
+                    height: 45,
+                    child: MyButton(
+                      BorderSide_color: Colors.black,
+                      text: "Sign Out",
+                      style:TextStyle(
+                        fontSize: 16,
+                        fontFamily: "Poppins",
+                        fontStyle: FontStyle.normal,
+                        color: Colors.black,
+                        fontWeight: FontWeight.w400,
+
+                      ),
+                      onPressed: (){},
+                    ),
+                  ),
+                  SizedBox(width: 45,),
+                  CircleAvatar(
+                    backgroundColor: HexColor("333333").withOpacity(0.1),
+                    child: Text("?",style: TextStyle(color: Colors.black,
+                        fontSize: 25,
+                        fontFamily: "Poppins",
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.normal),),
+                  ),
+                  SizedBox(width: 80,),
+                  NextArrow(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
