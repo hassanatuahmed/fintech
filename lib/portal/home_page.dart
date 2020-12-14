94a4406f5e7e9b1ebeda609d9bbab1e4ac1a8a85
hassanatuahmed <hassanatu@plendify.com>
2020-12-14 08:06:18 +0000

    monday

--- lib/portal/home_page.dart
+++ lib/portal/home_page.dart
@@ -26,7 +26,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _children[_currentIndex],
+
 
       bottomNavigationBar: BottomNavigationBar(currentIndex: _currentIndex,
         type: BottomNavigationBarType.fixed,
